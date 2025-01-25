de48315ee99385541e32a51240bb285e11a374d4
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2025-01-24 21:04:22 -0300

    feat: criando a entidade User

---
+++ src/main/java/br/com/redeSocialPB/models/User.java
@@ -0,0 +1,63 @@
+package br.com.redeSocialPB.models;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class User {
+
+    private UUID id;
+
+    private String nome;
+
+    private String email;
+
+    private String senha;
+
+    public User() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
