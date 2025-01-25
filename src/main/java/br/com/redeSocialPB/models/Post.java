b9b61b8d3d660446a1e9f3b007e6e12466477f8e
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2025-01-24 21:42:10 -0300

    chore: realizando o mapeamento do objeto relacional Post

--- src/main/java/br/com/redeSocialPB/models/Post.java
+++ src/main/java/br/com/redeSocialPB/models/Post.java
@@ -1,13 +1,24 @@
 package br.com.redeSocialPB.models;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 import java.util.UUID;
 
+@Entity
+@Table(name = "tb_posts")
 public class Post {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(name = "mensagem")
     private String mensagem;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
     public Post() {
     }
@@ -26,6 +37,14 @@ public class Post {
 
     public void setMensagem(String mensagem) {
         this.mensagem = mensagem;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 
     @Override
