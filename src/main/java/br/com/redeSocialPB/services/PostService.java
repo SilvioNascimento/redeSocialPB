40d9d7c3f04c5a39151f6a16e78ba1991f3be0ae
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2025-01-26 16:43:31 -0300

    feat: criando o PostService

---
+++ src/main/java/br/com/redeSocialPB/services/PostService.java
@@ -0,0 +1,59 @@
+package br.com.redeSocialPB.services;
+
+import br.com.redeSocialPB.models.Post;
+import br.com.redeSocialPB.repositories.PostRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    private PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public List<Post> getPosts() {
+        return postRepository.findAllWithComments();
+    }
+
+    public Post getPost(String id) {
+        return postRepository.findByIdWithComments(id)
+                .orElseThrow(() -> new RuntimeException("Post não encontrado!"));
+    }
+
+    public Post createPost(Post p) {
+        return postRepository.save(p);
+    }
+
+    public void deletePost(String id) {
+        Optional<Post> postOpt = postRepository.findById(id);
+        if(postOpt.isPresent()) {
+            postRepository.deleteById(id);
+            return;
+        }
+        throw new RuntimeException("Post com id " + id +
+                " não foi encontrado para ser deletado!");
+    }
+
+    public Post updatePost(String id, Post p) {
+        Optional<Post> postOpt = postRepository.findByIdWithComments(id);
+        if (postOpt.isPresent()) {
+            Post toUpdate = postOpt.get();
+
+            toUpdate.setMensagem(p.getMensagem());
+            toUpdate.setUser(p.getUser());
+
+            if (p.getComments() != null) {
+                toUpdate.setComments(p.getComments());
+            }
+
+            return postRepository.save(toUpdate);
+        }
+        throw new RuntimeException("Post com id " + id +
+                " não foi encontrado para ser atualizado!");
+    }
+}
