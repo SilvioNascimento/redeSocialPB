3726854855601477caea9ce4755ef2a61d9938e1
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2025-03-03 16:24:07 -0300

    feat: adicionando outros atributos importantes com validação

--- src/main/java/br/com/redeSocialPB/dto/RegisterRequestDTO.java
+++ src/main/java/br/com/redeSocialPB/dto/RegisterRequestDTO.java
@@ -1,13 +1,43 @@
 package br.com.redeSocialPB.dto;
 
+import br.com.redeSocialPB.validation.EmailsExistentes;
+import jakarta.validation.constraints.*;
+
 public class RegisterRequestDTO {
 
+    @NotNull
+    @NotBlank
+    @NotEmpty
+    private String nome;
+
+    @Email
+    @EmailsExistentes
+    private String email;
+
+    @NotNull
+    @NotBlank
+    @NotEmpty
     private String username;
+
+    @NotNull
+    @NotBlank
+    @NotEmpty
     private String senha;
 
-    public RegisterRequestDTO(String username, String senha) {
-        this.username = username;
-        this.senha = senha;
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getUsername() {
